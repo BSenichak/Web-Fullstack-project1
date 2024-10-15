03ff718f85c499e3af488d9573854779089217e4
BSenichak <bsenk77@gmail.com>
2024-10-15 12:36:51 +0300

    lesson 3

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const multer = require('multer');
 
 
 app.use(express.static('static'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+      cb(null, Date.now() + path.extname(file.originalname)); 
+    }
+  });
+  const upload = multer({ storage: storage });
+
 
 let products = []
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html');
+    res.render('index', { products: products });
 });
 
 app.post('/ads', (req, res) => {
@@ -18,11 +34,12 @@ app.post('/ads', (req, res) => {
     res.end(JSON.stringify(products));
 });
 
-app.post('/add', (req, res) => {
+app.post('/add', upload.single('image'), (req, res) => {
     let data = req.body;
+    data.image = req.file.filename;
     data.id = products.length;
     products.push(data);
-    res.redirect("/");
+    res.end()
 });
 
 app.listen(3000, () => {
--- static/script.js
+++ static/script.js
@@ -1,24 +1,17 @@
-let wrapper = document.querySelector(".wrapper");
-fetch("/ads", {
-    method: "post",
-}).then(async (response) => {
-    let ads = await response.json();
-    wrapper.innerHTML = "";
-    ads.forEach((product) => {
-        wrapper.innerHTML += `
-                <div class="ad">
-                    <h3>${product.title}</h3>
-                    <p>${product.description}</p>
-                    <p>${product.price}</p>
-                </div>
-                `;
-    });
-});
-
-
 document.querySelector("#add").addEventListener("click", () => {
     document.querySelector(".modal").style.display = "flex";
 });
 document.querySelector(".close").addEventListener("click", () => {
     document.querySelector(".modal").style.display = "none";
 });
+
+document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    fetch('/add', {
+        method: "POST",
+        body: new FormData(e.target) 
+    }).then(() => {
+        location.reload();
+    })
+
+})
