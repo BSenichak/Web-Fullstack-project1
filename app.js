712be82a32a0a04333a530b9e2ba2208e9c81ced
BSenichak <bsenk77@gmail.com>
2024-10-16 10:25:42 +0300

    add post page

--- app.js
+++ app.js
@@ -1,47 +1,48 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const multer = require('multer');
+const path = require("path");
+const multer = require("multer");
 
-
-app.use(express.static('static'));
+app.use(express.static("static"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.set("view engine", "ejs");
+app.set("views", "views");
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, 'uploads/');
+        cb(null, "uploads/");
     },
     filename: (req, file, cb) => {
-      cb(null, Date.now() + path.extname(file.originalname)); 
+        cb(null, Date.now() + path.extname(file.originalname));
+    },
+});
+const upload = multer({ storage: storage });
+
+let products = [];
+
+app.get("/", (req, res) => {
+    res.render("index", { products: products });
+});
+
+app.get("/post/:id", (req, res) => {
+    const postId = req.params.id;
+    if (!products[postId]) {  
+        return res.status(404).send();
     }
-  });
-  const upload = multer({ storage: storage });
-
-
-let products = []
-
-app.get('/', (req, res) => {
-    res.render('index', { products: products });
+    res.render("post", { product: products[postId] });
 });
 
-app.post('/ads', (req, res) => {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(products));
-});
-
-app.post('/add', upload.single('image'), (req, res) => {
+app.post("/add", upload.fields([{ name: "image" }]), (req, res) => {
     let data = req.body;
-    data.image = req.file.filename;
+    data.image = req.files.image.map((file) => file.filename);
     data.id = products.length;
     products.push(data);
-    res.end()
+    res.end();
 });
 
 app.listen(3000, () => {
-    console.log('http://localhost:3000');
+    console.log("http://localhost:3000");
 });
