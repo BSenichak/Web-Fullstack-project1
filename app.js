241a30ddc428d98f8f70725ee5e76453d832c884
BSenichak <bsenk77@gmail.com>
2024-10-23 09:49:30 +0300

    make it shorter

--- app.js
+++ app.js
@@ -15,14 +15,8 @@ app.get('/', (req, res) => {
     res.render('index', { products: products });
 });
 
-app.post('/ads', (req, res) => {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(products));
-});
-
 app.post('/add', (req, res) => {
     let data = req.body;
-    data.id = products.length;
     products.push(data);
     res.end()
 });
